9f0a9f9d34a594165a24055c31c412e6c293cf9e
Mike <nguyen.mike1092@gmail.com>
2024-01-30 23:33:09 -0500

    init commit

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+)
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    department INT,
+    salary INT,
+    FOREIGN KEY (department) REFERENCES department(id) ON DELETE SET NULL,
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    job_titles VARCHAR(30) NOT NULL,
+    department INT,
+    salaries INT,
+    manager INT,
+    FOREIGN KEY (department) REFERENCES department(id) ON DELETE SET NULL,
+    FOREIGN KEY (salaries) REFERENCES roles(salary) ON DELETE SET NULL
+)
